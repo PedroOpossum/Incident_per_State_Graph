6d258458ed26aea13513d630d3789c034d2fb44f
pedro-opossum <mason.s.morales2005@gmail.com>
2025-12-10 22:15:41 -0600

    N/A for states

--- Networkx.py
+++ Networkx.py
@@ -7,6 +7,7 @@ from io import StringIO
 
 urlState = "https://www.bls.gov/iif/state-data/archive.htm#NJ"
 
+Yearinput = input("Enter the year you want to view between 2023-2011: " )
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
@@ -21,6 +22,7 @@ try:
     df = data[0]
     df = df[["State", "Fatal Injury Counts"]]
 
+
     states = (
         df["State"]
         .str.lower()
@@ -29,9 +31,20 @@ try:
         .tolist()
     )
 
+    #New York state has different URL so it needs to be changed
+
     for stateLoop in states:
-        urlInjury = f"https://www.bls.gov/iif/state-data/fatal-occupational-injuries-in-{stateLoop}-2023.htm"
-        print(urlInjury)
+        urlInjury = f"https://www.bls.gov/iif/state-data/fatal-occupational-injuries-in-{stateLoop}-{Yearinput}.htm"
+        injuryTable = requests.get(urlInjury, headers=headers, timeout = 5)
+        tables = pd.read_html(StringIO(injuryTable.text))
+        df = tables[0]
+
+        if "Year" in df.columns:
+            print(f"{stateLoop}: N/A")
+            continue
+
+        totalInjuries = df.iloc[0, 1]
+        print(f"{stateLoop}: {totalInjuries}")
 
 
 
