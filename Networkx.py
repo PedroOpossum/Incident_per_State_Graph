8ef6881f1de2b1c156914276d69e100823d11146
pedro-opossum <mason.s.morales2005@gmail.com>
2025-12-13 19:04:13 -0600

    New York City issue

--- Networkx.py
+++ Networkx.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd;
 import requests
 from io import StringIO
-
+from bs4 import BeautifulSoup
 
 urlState = "https://www.bls.gov/iif/state-data/archive.htm#NJ"
 
-Yearinput = input("Enter the year you want to view between 2023-2011: " )
+Yearinput = input("Enter the year you want to view between 2023-2020: " ).strip()
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
@@ -17,21 +17,32 @@ headers = {
 
 
 try:
+
+    
     stateTable = requests.get(urlState, headers=headers, timeout = 5)
     data = pd.read_html(StringIO(stateTable.text))
     df = data[0]
-    df = df[["State", "Fatal Injury Counts"]]
+    df = df[["State"]]
 
 
     states = (
         df["State"]
         .str.lower()
+
+        #Skips "all participating states" row
         .iloc[1:]
+        
+        #The URL doesn't work for new york state, so we have to rename it, plus remove New York city since it isnt a state
+        .replace({"new york state": "new york state including new york city", "new york city": pd.NA}) 
+
+        # remove the NaN (NYC)
+        .dropna()  
+
+        #Converts it into a URL
         .str.replace(" ", "-")
         .tolist()
     )
 
-    #New York state has different URL so it needs to be changed
 
     for stateLoop in states:
         urlInjury = f"https://www.bls.gov/iif/state-data/fatal-occupational-injuries-in-{stateLoop}-{Yearinput}.htm"
@@ -39,10 +50,12 @@ try:
         tables = pd.read_html(StringIO(injuryTable.text))
         df = tables[0]
 
+        #If the page doesn't exists it goes to https://www.bls.gov/iif/, where it displays "year" in the table. 
         if "Year" in df.columns:
             print(f"{stateLoop}: N/A")
             continue
 
+        #Grabs the total fatal injuries from each state
         totalInjuries = df.iloc[0, 1]
         print(f"{stateLoop}: {totalInjuries}")
 
